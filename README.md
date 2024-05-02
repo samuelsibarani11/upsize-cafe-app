fee9c30359794d36c099bfa17c66371cbacb136f
samuelsibarani11 <samuelalbi09@gmail.com>
2024-05-02 09:28:48 +0700

    tes123

---
+++ README.md
@@ -0,0 +1,1 @@
+# upsize-cafe-app
---
+++ upsize-cafe-app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace upsize_cafe_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
