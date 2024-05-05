075a6ef547416d0ebd8531c9b98f54c18a71917a
Samuel Albi Pulo Sibarani <130736394+samuelsibarani11@users.noreply.github.com>
2024-05-05 14:21:41 +0700

    Delete upsize-cafe-app directory

--- upsize-cafe-app/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace upsize_cafe_app.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
